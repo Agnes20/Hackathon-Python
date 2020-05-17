ba0c1ec1b1daf55fa22309f331fea7d68651a6f2
Agnes20 <agnes19gomez@gmail.com>
2020-05-17 11:05:11 +0200

    marter

--- Kata4/main.py
+++ Kata4/main.py
@@ -1,69 +1,98 @@
-import pygame, random
+import pygame, sys, random
 
-screent_width = 1280
-screent_height = 960
+def ball_animation():
+	global ball_speed_x, ball_speed_y
+	
+	ball.x += ball_speed_x
+	ball.y += ball_speed_y
 
-#colors
-white_color= (200,200,200)
-light_gray = pygame.Color('grey12')
+	if ball.top <= 0 or ball.bottom >= screen_height:
+		ball_speed_y *= -1
+	if ball.left <= 0 or ball.right >= screen_width:
+		ball_start()
 
+	if ball.colliderect(player) or ball.colliderect(opponent):
+		ball_speed_x *= -1
+
+def player_animation():
+	player.y += player_speed
+
+	if player.top <= 0:
+		player.top = 0
+	if player.bottom >= screen_height:
+		player.bottom = screen_height
+
+def opponent_ai():
+	if opponent.top < ball.y:
+		opponent.y += opponent_speed
+	if opponent.bottom > ball.y:
+		opponent.y -= opponent_speed
+
+	if opponent.top <= 0:
+		opponent.top = 0
+	if opponent.bottom >= screen_height:
+		opponent.bottom = screen_height
+
+def ball_start():
+	global ball_speed_x, ball_speed_y
+
+	ball.center = (screen_width/2, screen_height/2)
+	ball_speed_y *= random.choice((1,-1))
+	ball_speed_x *= random.choice((1,-1))
+
+# General setup
 pygame.init()
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((screent_width, screent_height))
+# Main Window
+screen_width = 1280
+screen_height = 960
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Pong')
 
-def mover_rectangulo():
-    global spped #permite acceder funciones de fuera
-    if rectangulo.top + 50 < screent_height:
-        rectangulo.top += spped
+# Colors
+light_grey = (200,200,200)
+bg_color = pygame.Color('grey12')
 
-def start_bola():
-    global spped_bola_x #permite acceder funciones de fuera
-    global spped_bola_y #permite acceder funciones de fuera
-    if bola.left + 50 > screent_width or bola.top <0:
-        bola.top = screent_height // 2
-        bola.left = screent_width // 2
-    spped_bola_x = 3*random.choice((1,-1))
-    spped_bola_y = 3*random.choice((1,-1))
+# Game Rectangles
+ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10,140)
+opponent = pygame.Rect(10, screen_height / 2 - 70, 10,140)
 
+# Game Variables
+ball_speed_x = 7 * random.choice((1,-1))
+ball_speed_y = 7 * random.choice((1,-1))
+player_speed = 0
+opponent_speed = 7
 
-def mover_bola():
-    global spped_bola_x #permite acceder funciones de fuera
-    global spped_bola_y #permite acceder funciones de fuera
-    if bola.top + 50 > screent_height or bola.top <0:
-        spped_bola_x = -spped_bola_x
-
-    if bola.left < 10 and rectangulo.top < bola.top >rectangulo.top +140:
-        spped_bola_y = -spped_bola_y
-
-    start_bola()
-
-    bola.top += spped_bola_x
-    bola.left+= spped_bola_y
-
-
-rectangulo = pygame.Rect(10,10,10,140)
-bola = pygame.Rect(50,10,50,50)
-spped = 0
-spped_bola_x = 3
-spped_bola_y = 3
 while True:
-    screen.fill(light_gray)
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                spped = -3
-            elif event.hey == pygame.K_DOWN:
-                spped = 3
-            elif event.hey == pygame.KEYUP:
-                spped = 0
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				player_speed -= 6
+			if event.key == pygame.K_DOWN:
+				player_speed += 6
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_UP:
+				player_speed += 6
+			if event.key == pygame.K_DOWN:
+				player_speed -= 6
+	
+	#Game Logic
+	ball_animation()
+	player_animation()
+	opponent_ai()
 
-    mover_rectangulo()
-    mover_bola()
-    #dibujar
-    pygame.draw.rect(screen,white_color, rectangulo)
-    #dibujar
-    pygame.draw.ellipse(screen,white_color, bola)
-    pygame.display.flip() #refescar pantalla
-    clock.tick(60)#calcula tick 60 frame por segundo
+	# Visuals 
+	screen.fill(bg_color)
+	pygame.draw.rect(screen, light_grey, player)
+	pygame.draw.rect(screen, light_grey, opponent)
+	pygame.draw.ellipse(screen, light_grey, ball)
+	pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0),(screen_width / 2, screen_height))
+
+	pygame.display.flip()
+	clock.tick(60) #calcula a 60 frames por segundo
 print("Kata4")
